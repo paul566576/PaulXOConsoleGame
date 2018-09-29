983e0abc157d26f50281f52b8f1f19525ff3eb62
paul566576 <slimm566576@gmail.com>
2018-09-29 18:06:38 +0300

    ss

---
+++ src/io/hexlet/xo/controller/WinnerController.java
@@ -0,0 +1,67 @@
+package io.hexlet.xo.controller;
+
+import io.hexlet.xo.model.Field;
+import io.hexlet.xo.model.Figure;
+import io.hexlet.xo.model.exception.InvalidPointException;
+
+import java.awt.*;
+
+public class WinnerController {
+
+    public Figure getWinner(final Field field) {
+        try {
+            for (int i = 0; i < 3; i++) {
+                if (check(field, new Point(i,0), p -> new Point(p.x, p.y + 1))) {
+                    return field.getFigure(new Point(i,0));
+                }
+            }
+
+            for (int i = 0; i < 3; i++) {
+                if (check(field, new Point(0,i), p -> new Point(p.x + 1, p.y))) {
+                    return field.getFigure(new Point(0, i));
+                }
+            }
+
+            for (int i = 0; i < 3; i++) {
+                if (check(field, new Point(0,0), p -> new Point(p.x + 1, p.y + 1))) {
+                    return field.getFigure(new Point(0,0));
+                }
+            }
+
+            for (int i = 0; i < 3; i++) {
+                if (check(field, new Point(0,2), p -> new Point(p.x + 1, p.y - 1))) {
+                    return field.getFigure(new Point(1, 1));
+                }
+            }
+        } catch(InvalidPointException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public boolean check(final Field field,
+                         final Point currentPoint,
+                         final IPointGenerator pointGenerator) {
+        final Figure currenFigure;
+        final Figure nextFigure;
+        final Point nextPoint = pointGenerator.next(currentPoint);
+        try {
+            currenFigure = field.getFigure(currentPoint);
+
+            if (currenFigure == null) return false;
+
+            nextFigure = field.getFigure(nextPoint);
+        }catch (InvalidPointException e) {
+            return true;
+        }
+
+        if (currenFigure != nextFigure) return false;
+
+        return check(field, nextPoint, pointGenerator);
+    }
+
+    private interface IPointGenerator {
+
+        Point next(final Point point);
+    }
+}
---
+++ src/io/hexlet/xo/model/Field.java
@@ -0,0 +1,46 @@
+package io.hexlet.xo.model;
+
+import io.hexlet.xo.model.exception.InvalidPointException;
+
+import java.awt.*;
+
+public class Field {
+
+    private static final int MIN_COORDINATE = 0;
+
+    private final int fieldSize;
+
+    private final Figure[][] field;
+
+    public Field(final int fieldSize) {
+        this.fieldSize = fieldSize;
+        this.field = new Figure[this.fieldSize][this.fieldSize];
+    }
+
+    public int getSize() {
+        return this.fieldSize;
+    }
+
+    public Figure getFigure(final Point point) throws InvalidPointException {
+        if(!checkPoint(point)) {
+            throw new InvalidPointException();
+        }
+        return field[point.x][point.y];
+    }
+
+    public void setFigure(final Point point, final Figure figure) throws InvalidPointException {
+        if(!checkPoint(point)) {
+            throw new InvalidPointException();
+        }
+
+        field[point.x][point.y] = figure;
+    }
+
+    private boolean checkPoint(final Point point) {
+        return checkCoordinate(point.x, field.length) && checkCoordinate(point.y, field.length);
+    }
+
+    private boolean checkCoordinate(final int coordinate, final int maxCoordinate) {
+        return coordinate >= MIN_COORDINATE && coordinate < maxCoordinate;
+    }
+}
